52a50edde4fed5d5a383182a7dbe8e16bc57ae88
dfinazhin <dfinazhin@gmail.com>
2025-09-29 02:07:53 +0500

    Форма переделана на HookForm и Yup

--- src/components/login-form/login-form.jsx
+++ src/components/login-form/login-form.jsx
@@ -1,7 +1,7 @@
-import { useStore } from '../../hooks/useStore';
-import { useFormErrors } from '../../hooks/useFormErrors';
 import { useEffect, useRef } from 'react';
-import { validateFieldsLimits } from './utilities/validate-fields-limits';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { fieldsSchema } from './fields-schema';
 import styles from './login-form.module.css';
 
 const sendFormData = (formData) => {
@@ -9,131 +9,63 @@ const sendFormData = (formData) => {
 };
 
 export const LoginForm = () => {
-    const { getState, updateState, resetState } = useStore();
-    const { getFormErrors, updateFormErrors } = useFormErrors();
     const submitButtonRef = useRef(null);
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        sendFormData(getState());
-        resetState();
-    };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            email: '',
+            password: '',
+            repeatedPassword: '',
+        },
+        resolver: yupResolver(fieldsSchema),
+    });
 
-    const { email, password, repeatedPassword } = getState();
-
-    const onChange = ({ target }) => {
-        updateState(target.name, target.value);
-
-        let newError = null;
-
-        switch (target.name) {
-            case 'email':
-                newError = validateFieldsLimits(target.name, target.value, true);
-                break;
-            case 'password':
-                newError = validateFieldsLimits(target.name, target.value, true);
-                break;
-            case 'repeatedPassword':
-                newError = validateFieldsLimits(target.name, target.value, true);
-                break;
-            default:
-                break;
-        }
-        updateFormErrors(target.name, newError);
-    };
-
-    const onBlur = ({ target }) => {
-        let newError = null;
-        switch (target.name) {
-            case 'email':
-                newError = validateFieldsLimits(target.name, target.value, false);
-                break;
-            case 'password':
-                newError = validateFieldsLimits(target.name, target.value, false);
-                break;
-            case 'repeatedPassword':
-                if (getState().password !== getState().repeatedPassword) {
-                    newError = 'Пароли не совпадают';
-                } else {
-                    newError = validateFieldsLimits(target.name, target.value, false);
-                }
-                break;
-            default:
-                break;
-        }
-        updateFormErrors(target.name, newError);
-    };
-
-    const isDisabledSubmit = () => {
-        return (
-            getFormErrors().email ||
-            getFormErrors().password ||
-            getFormErrors().repeatedPassword ||
-            !getState().email ||
-            !getState().password ||
-            !getState().repeatedPassword ||
-            getState().password !== getState().repeatedPassword
-        );
-    };
-
-    const isFormValid = isDisabledSubmit();
+    let formErrors;
+    if (errors.email?.message) {
+        formErrors = errors.email?.message;
+    } else if (errors.password?.message) {
+        formErrors = errors.password?.message;
+    } else if (errors.repeatedPassword?.message) {
+        formErrors = errors.repeatedPassword?.message;
+    }
 
     useEffect(() => {
-        if (!isFormValid) {
+        if (!formErrors) {
             submitButtonRef.current.focus();
         }
-    }, [isFormValid]);
+    }, [formErrors]);
 
     return (
         <>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit(sendFormData)}>
                 <input
                     type="email"
                     name="email"
-                    value={email}
                     placeholder="Почта"
-                    onChange={onChange}
-                    onBlur={onBlur}
+                    {...register('email')}
                 />
                 <input
                     type="password"
                     name="password"
-                    value={password}
                     placeholder="Пароль"
-                    onChange={onChange}
-                    onBlur={onBlur}
+                    {...register('password')}
                 />
                 <input
                     type="password"
                     name="repeatedPassword"
-                    value={repeatedPassword}
                     placeholder="Повторите пароль"
-                    onChange={onChange}
-                    onBlur={onBlur}
+                    {...register('repeatedPassword')}
                 />
-                <button type="button" onClick={resetState}>
-                    Сброс
-                </button>
-                <button
-                    type="submit"
-                    disabled={isDisabledSubmit()}
-                    ref={submitButtonRef}
-                >
+                <button type="submit" disabled={!!formErrors} ref={submitButtonRef}>
                     Отправить
                 </button>
             </form>
-            {getFormErrors().email && (
-                <div className={styles['error-message']}>{getFormErrors().email}</div>
-            )}
-            {getFormErrors().password && (
-                <div className={styles['error-message']}>
-                    {getFormErrors().password}
-                </div>
-            )}
-            {getFormErrors().repeatedPassword && (
-                <div className={styles['error-message']}>
-                    {getFormErrors().repeatedPassword}
-                </div>
+            {formErrors && (
+                <div className={styles['error-message']}>{formErrors}</div>
             )}
         </>
     );
